f09e0a1e8ffa24447d2e1f39f11e8084c05b060a
Gustavo Bacellar <61608608+gustavobacellarladeira@users.noreply.github.com>
2026-02-05 20:53:38 -0300

    Refactor code structure for improved readability and maintainability

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import App from '../../App';
 
 // Use fake timers to prevent timer-related warnings
 jest.useFakeTimers();
